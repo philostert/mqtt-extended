6929f2fc351758069313f9ee39b1ab4d69ec36c8
Philipp Ostertag <ostp@posteo.de>
2016-02-07 22:37:51 +0100

    other type of Subscription Tracker

--- broker/tracker.py
+++ broker/tracker.py
@@ -1,4 +1,12 @@
 from broker.exceptions import BeginTracking, EndTracking
+import re
+
+# Two basic alternatives "one-topic-level" and "at-least-two-topic-levels" in order to not match empty strings.
+# Per level literal character group %(l)s excludes certain unicode points.
+# Unicode exclusions defined @ MQTT Version 3.1.1, Section 1.5.3 UTF-8 encoded strings.
+re_match_valid_mask = re.compile(r'^([+#]|%(l)s+)$|^(([+]|%(l)s*)/(([+]|%(l)s*)/)*([+#]|%(l)s*))$' %
+                                 {'l': r'[^+#/\u0000\ud800-\udfff\u0001-\u001f\u007f-\u009f]'}) # group of literals
+
 
 class Tracker:
     def __init__(self):
@@ -9,12 +17,53 @@ class TopicTracker(Tracker):
     def __init__(self):
         super().__init__()
 
+def foo(mask, qos, client):
+    pass
+    topic_levels = mask.split("/")
+
+
 # FIXME don't ask, tell! Let the tracker also do forwarding decisions and their execution?!
 class SubMaskTracker(Tracker):
-    class _SubMaskTrackerEntry:
-        def __init__(self, engine):
-            self._engine = engine
-            self._subs = dict()
+    def add_sub(mask, qos, client):
+        assert isinstance(mask, str)
+        assert isinstance(qos, int)
+        if not re_match_valid_mask.match(mask):
+            raise ValueError
+
+        topic_levels = mask.split("/")
+        for level in topic_levels:
+            self.
+
+    class _LevelEntry:
+        def __init__(self, level_name):
+            self._sublevels = dict()        # topic level key e.g. '+', '#', 'temperature'  => _LevelEntry
+            self._subscriptions = dict()    # client.uid  => qos
+            self._match_all = False
+            if level_name == '#':
+                self._match_all = True
+
+        def get_subscriptions(self, subsequent_levels):
+            if not subsequent_levels or self._match_all:
+                return self._subscriptions.copy()
+            else:
+                assert (subsequent_levels, list)
+                # cut list and determine next sub levels to walk
+                subsequent_levels = subsequent_levels.copy()
+                next_literal = subsequent_levels.pop(0)
+                next_levels = {key for key in {'+', '#', next_literal} if key in self._sublevels}
+
+                # create new mapping where all subscriptions are merged
+                matches = type(self._subscriptions)()
+                for key in next_levels:
+                    additional_matches = self._sublevels[key].get_subscriptions(subsequent_levels)
+                    # get maximum qos for each
+                    for client, qos in additional_matches:
+                        if client not in matches or qos > matches[client]:
+                            matches[client] = qos
+                return matches
+
+
+
 
         def add_client(self, qos, uid):
             assert isinstance(qos, int)
